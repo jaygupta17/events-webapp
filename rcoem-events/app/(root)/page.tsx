bbc35e2e5fa6436cf2e7ba495b15394938c19b7c
Jay Gupta <146335736+jaygupta17@users.noreply.github.com>
2024-06-15 17:56:35 +0000

    q

--- rcoem-events/app/(root)/page.tsx
+++ rcoem-events/app/(root)/page.tsx
@@ -14,7 +14,7 @@ if(!user) redirect("/login")
         <div className="h-[90dvh] flex justify-center items-center gap-y-2 flex-col">
             {user.labels.includes("ORGANISER") && <Link href="/create-event" className="flex justify-center rounded-full font-semibold fixed bottom-10 right-4 p-3 bg-blue-500/70"><PlusIcon className="text-white/80" size={35}/></Link> }
             <div className="h-full  w-[90%] flex justify-center items-center">
-                Nothing to see here...
+                Nothing to see here hehe...
             </div>
         </div>
     )
